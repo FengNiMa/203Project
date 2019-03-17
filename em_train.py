0df1d55ebc1abf5081999ecb78064dfc80dfef37
Sophia Sun <sophiaphia@live.com>
2019-03-17 13:45:51 -0700

    em train error handling and mnist exp

--- em_train.py
+++ em_train.py
@@ -1,5 +1,4 @@
 import random as rand
-
 import numpy as np
 import scipy as sp
 from scipy.stats import multivariate_normal as mvn
@@ -7,12 +6,24 @@ import os, sys, time, pickle
 from tqdm import tqdm
 
 def log_likelihood(X, pi, mu, sigma):
+    k = len(pi) # dimention
+    d = sigma.shape[1]
     ll = 0.0
     for x in X:
-        s = 0
-        for j in range(len(pi)):
-            s += pi[j] * mvn(mu[j], sigma[j]).pdf(x)
-        ll += np.log(s)
+       
+        try:
+            s = 0
+            for j in range(k):
+                s += pi[j] * mvn(mu[j], sigma[j]).pdf(x)
+            ll += np.log(s)
+        except Exception as e:
+            s = 0
+            for j in range(k):
+                cov2 = np.matmul(cov[k].T, cov[k]) + torch.eye(d) * 1e-3 # min eig
+                sign, logdet = np.linalg.slogdet(cov2)
+                s += -d * 0.5 * math.log(2 * math.pi) - 0.5 * sign*logdet - 0.5 * np.linalg.inv(cov2).matmul(x - mu[j]).dot(x - mu[j])
+            ll += s
+
     return ll
 
 def penalize_term(X, Z, pi, mu, sigma, lam):
@@ -33,7 +44,7 @@ def em_gmm(X, pi, mu, sigma, tol=1e-6, max_iter=1000):
     losses = [ll_old]
     step_iterator = tqdm(range(max_iter))
     for i_iter in step_iterator:
-        #print('Iteration %d: log-likelihood is %.6f'%(i_iter, ll_old))
+        #print('EM Iteration %d: log-likelihood is %.6f'%(i_iter, ll_old))
 
         # E-step
         w = np.zeros((k, n))
@@ -58,7 +69,7 @@ def em_gmm(X, pi, mu, sigma, tol=1e-6, max_iter=1000):
         sigma = np.zeros((k, p, p))
         for j in range(k):
             for i in range(n):
-                ys = np.reshape(X[i]- mu[j], (2,1))
+                ys = np.reshape(X[i]- mu[j], (p,1))
                 sigma[j] += w[j, i] * np.dot(ys, ys.T)
             sigma[j] /= w[j,:].sum()
 
@@ -114,13 +125,13 @@ def em_gmm_penalized(X, Z, pi, mu, sigma, lmda=1, tol=1e-6, max_iter=1000):
         sigma = np.zeros((k, p, p))
         for j in range(k):
             for i in range(n):
-                ys = np.reshape(X[i]- mu[j], (2,1))
+                ys = np.reshape(X[i]- mu[j], (p,1))
                 sigma[j] += w[j, i] * np.dot(ys, ys.T)
             sigma[j] /= w[j,:].sum()
 
         # M-iter
         old_mu, old_sigma = mu, sigma
-        for inner_iter in range(10):
+        for inner_iter in range(30):
             gamma = np.zeros(k)
             for j in range(k):
                 for z in Z:
@@ -143,7 +154,7 @@ def em_gmm_penalized(X, Z, pi, mu, sigma, lmda=1, tol=1e-6, max_iter=1000):
                     sigma[j] += w[j, i] * np.dot(ys, ys.T)
                 sigma[j] /= w[j,:].sum()
             
-            if np.linalg.norm(old_mu - mu) + np.linalg.norm(old_sigma - sigma)  < 1e-16: #convergence criterion
+            if np.linalg.norm(old_mu - mu) + np.linalg.norm(old_sigma - sigma)  < 1e-10: #convergence criterion
                 old_mu, old_sigma = mu, sigma
                 break
 
@@ -171,21 +182,22 @@ def em_gmm_penalized(X, Z, pi, mu, sigma, lmda=1, tol=1e-6, max_iter=1000):
 if __name__ == '__main__':
     
     output_dir = 'results'
-    dataset_name = 'multi-adv-2'
+    dataset_name = 'MNIST'
     os.makedirs(os.path.join(output_dir, dataset_name, 'EM'), exist_ok=True)
 
-    data_fname = os.path.join('datasets', dataset_name, 'data_multi_adv_1000.npz')
+    data_fname = os.path.join('datasets', dataset_name, 'mnist_1797_afterPCA.npz')
     load_data = np.load(data_fname)
-    N = 1000
-    true_pi = load_data['pi']
-    true_mu = load_data['mu']
-    X = load_data['samples'][:N]
-    Z = load_data['adv_sample']
-    data_range = 6.0
+    N = 1797
+    #true_pi = load_data['pi']
+    #true_mu = load_data['mu']
+    X = load_data['data'][:N]
+    #Z = load_data['adv_sample']
+    data_range = 1.0
     
-    exps = 6
-    lam_settings = [0.1, 1.0, 10.0, 100.0]
-    K_settings = [3, 5, 10]
+    d = X.shape[1]
+    exps = 2
+    lam_settings = [0] #[0.1, 1.0, 10.0, 100.0]
+    K_settings = [2, 5, 7, 10, 15]
     all_settings = [(K, lam) for lam in lam_settings for K in K_settings]
 
     for K, lam in all_settings:
@@ -193,37 +205,36 @@ if __name__ == '__main__':
         em_p_results = []
         trials = 0
         for e in range(exps):
-            try:
-                print("k:", K,"lam:", lam)
-                # initial guesses for parameters
-                pis = np.ones(K)
-                pis /= pis.sum()
-                mus = np.random.random((K,2)) * data_range #range is [0,6]x[0,6]
-                sigmas = np.array([np.eye(2)] * K)
 
-                start_t = time.time()
-                pi, mu, conv, losses, iter_n = em_gmm(X, pis, mus, sigmas)
-                em_results.append({"init_guess":[pis, mus, sigmas],
-                                    "pi":pi, "mu":mu, "conv":conv,
-                                    "loss":losses, "iters":iter_n, 
-                                    "time":time.time()-start_t})
-                
-                start_t = time.time()
-                pi, mu, conv, p_loss, d_loss, inner_iter = em_gmm_penalized(X, Z, pis, mus, sigmas)
-                em_p_results.append({"init_guess":[pis, mus, sigmas],
-                                    "pi":pi, "mu":mu, "conv":conv, 
-                                    "p_loss":p_loss, "d_loss":d_loss, 
-                                    "iters":inner_iter,
-                                    "time":time.time()-start_t})
-                trials += 1
-                if trials >= 3:
-                    break
-            except Exception as e:
-                print("Error")
-                print(e)
+            print("k:", K,"lam:", lam)
+            # initial guesses for parameters
+            pis = np.ones(K)
+            pis /= pis.sum()
+            mus = np.random.random((K,d)) * data_range 
+            sigmas = np.array([np.eye(d)] * K)
+
+            start_t = time.time()
+            pi, mu, conv, losses, iter_n = em_gmm(X, pis, mus, sigmas)
+            em_results.append({"init_guess":[pis, mus, sigmas],
+                                "pi":pi, "mu":mu, "conv":conv,
+                                "loss":losses, "iters":iter_n, 
+                                "time":time.time()-start_t})
+
+
+
+            start_t = time.time()
+            pi, mu, conv, p_loss, d_loss, inner_iter = em_gmm_penalized(X, Z, pis, mus, sigmas)
+            em_p_results.append({"init_guess":[pis, mus, sigmas],
+                                "pi":pi, "mu":mu, "conv":conv, 
+                                "p_loss":p_loss, "d_loss":d_loss, 
+                                "iters":inner_iter,
+                                "time":time.time()-start_t})
+            trials += 1
+            if trials >= 3:
+                break
+
         
         with open(os.path.join(output_dir, dataset_name, 'EM', 'EM-K={}-lam={}-N={}.p'.format(K, lam, N)), 'wb') as p:
             pickle.dump(em_results, p)
-
         with open(os.path.join(output_dir, dataset_name, 'EM', 'Penalized-K={}-lam={}-N={}.p'.format(K, lam, N)), 'wb') as p:
             pickle.dump(em_p_results, p)
